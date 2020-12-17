24490fe5dfc5d6b9eb30b47eeda55646fce93be8
Lailz <lailaabdulraheem@gmail.com>
2020-12-17 10:06:54 +0300

    cleanup

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,29 +1,9 @@
 // ACTION TYPES
-const CREATE_COOKIE = "CREATE_COOKIE";
 const DELETE_COOKIE = "DELETE_COOKIE";
-const UPDATE_COOKIE = "UPDATE_COOKIE";
-
-export const createCookie = (newCookie) => {
-  return {
-    type: CREATE_COOKIE,
-    payload: { newCookie },
-  };
-};
 
 export const deleteCookie = (cookieId) => {
   return {
     type: DELETE_COOKIE,
     payload: { cookieId },
-  };
-};
-
-export const updateCookie = (updatedCookie) => {
-  console.log(
-    "🚀 ~ file: actions.js ~ line 25 ~ updateCookie ~ updatedCookie",
-    updatedCookie
-  );
-  return {
-    type: CREATE_COOKIE,
-    payload: { updatedCookie },
   };
 };
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,4 @@
 import cookiesData from "../cookies";
-import slugify from "react-slugify";
 
 const initialState = {
   cookies: cookiesData,
@@ -7,15 +6,6 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "CREATE_COOKIE":
-      const { newCookie } = action.payload;
-      newCookie.id = state.cookies[state.cookies.length - 1].id + 1;
-      newCookie.slug = slugify(newCookie.name);
-      return {
-        ...state,
-        cookies: [...state.cookies, newCookie],
-      };
-
     case "DELETE_COOKIE":
       return {
         ...state,
